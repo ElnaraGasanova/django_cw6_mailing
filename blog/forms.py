8d7925bc7c4b14d0f531abe00f934ebd9d0c7556
ElnaraGasanova <elnaraskypro@gmail.com>
2024-05-16 00:44:38 +0300

    Оформлена стилизация формы.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from blog.models import Blog
+from mailing.forms import StyleFormMixin
+
+
+class BlogForm(StyleFormMixin, forms.ModelForm):
+    '''Класс стилизации формы Блога.'''
+    class Meta:
+        model = Blog
+        exclude = ('preview',)
