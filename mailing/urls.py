c3d07bf1c5ec7c7e8b355ac1b9751e8c2d0b25c1
ElnaraGasanova <elnaraskypro@gmail.com>
2024-05-18 10:07:26 +0300

    Добавила закладку контакты в главное меню. Форма отправки работает, сообщение приходит!

---
+++ mailing/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from mailing.apps import MailingConfig
+from mailing.views import (IndexView, ClientListView, ClientCreateView, ClientUpdateView,ClientDeleteView,
+                           MessageListView, MessageCreateView, MessageUpdateView, MessageDeleteView, MailingListView,
+                           MailingDetailView, MailingCreateView, MailingUpdateView, MailingDeleteView, contacts)
+
+app_name = MailingConfig.name
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('contacts/', contacts, name='contacts'),
+
+    path('client/', ClientListView.as_view(), name='client_list'),
+    path('client/create/', ClientCreateView.as_view(), name='create_client'),
+    path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='update_client'),
+    path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='delete_client'),
+
+    path('message/', MessageListView.as_view(), name='message_list'),
+    path('message/create/', MessageCreateView.as_view(), name='create_message'),
+    path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='update_message'),
+    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='delete_message'),
+
+    path('mail/', MailingListView.as_view(), name='mail_list'),
+    path('mail/view/<int:pk>/', MailingDetailView.as_view(), name='view_mail'),
+    path('mail/create/', MailingCreateView.as_view(), name='create_mail'),
+    path('mail/update/<int:pk>/', MailingUpdateView.as_view(), name='update_mail'),
+    path('mail/delete/<int:pk>/', MailingDeleteView.as_view(), name='delete_mail'),
+]
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,125 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from blog.models import Blog
+from mailing.forms import MailingForm, ClientForm, MessageForm
+from mailing.models import Mailing, Client, Message, Log
 
-# Create your views here.
+
+class MailingListView(LoginRequiredMixin, ListView):
+    model = Mailing
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+
+
+class MailingCreateView(LoginRequiredMixin, CreateView):
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class MailingUpdateView(LoginRequiredMixin, UpdateView):
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner or self.request.user.is_superuser:
+            return MailingForm
+        else:
+            return self.get_form_class()
+
+
+class MailingDeleteView(LoginRequiredMixin, DeleteView):
+    model = Mailing
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class IndexView(TemplateView):
+    template_name = 'mailing/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['article_list'] = Blog.objects.all()[:3]
+        context['object_list'] = Mailing.objects.all()
+
+        unique_clients_count = Client.objects.all().values('email').distinct().count()
+        context['unique_clients_count'] = unique_clients_count
+
+        active_mailing_count = Mailing.objects.filter(is_active=True).count()
+        context['active_mailing_count'] = active_mailing_count
+        return context
+
+def contacts(request):
+    context = {
+        'title': 'Контакты',
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"{name} ({phone}, {email}): {message}")
+
+    return render(request, 'mailing/contacts.html', context)
+
+
+class LogListView(ListView):
+    model = Log
+    success_url = reverse_lazy('mailing:mailing_list')
