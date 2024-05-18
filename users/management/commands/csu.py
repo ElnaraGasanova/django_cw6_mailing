dc5bad7f4cbe05372d3eb44eafabc6b8b6db473b
ElnaraGasanova <elnaraskypro@gmail.com>
2024-05-18 15:34:42 +0300

    Superuser.

---
+++ users/management/commands/csu.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='skypro.msk@yandex.ru',
+            first_name='Admin',
+            last_name='Adminov',
+            is_superuser=True,
+            is_staff=True,
+            is_active=True
+        )
+        user.set_password('QWErty111')
+        user.save()
+
+#авториз.пользователь
+#skypro.msk@yandex.ru
+#Django@147
